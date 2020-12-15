96038beae665b095ed92fd0af662e9c03cff0bd9
Yuhyeon95 <71800820+Yuhyeon95@users.noreply.github.com>
2020-12-15 16:49:05 +0900

    setting_1

---
+++ src/main/java/com/newneek/j1/main/HomeController.java
@@ -0,0 +1,13 @@
+package com.newneek.j1.main;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	
+	@GetMapping("/")
+	public String home() {
+		return "index";
+	}
+}
