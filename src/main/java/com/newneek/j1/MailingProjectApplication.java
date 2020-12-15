0862ec4e0c69b4e4e8a2153075a844024dcefe07
jiseung-jung <jiseung0430@naver.com>
2020-12-15 15:13:20 +0900

    Initial commit

---
+++ src/main/java/com/newneek/j1/MailingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.newneek.j1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailingProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailingProjectApplication.class, args);
+	}
+
+}
