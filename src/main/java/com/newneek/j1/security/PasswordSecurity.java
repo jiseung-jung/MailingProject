e192a13566a7764457eed84dffc931f936b90b9f
jiseung-jung <jiseung0430@naver.com>
2021-01-15 17:04:36 +0900

    update-security

--- src/main/java/com/newneek/j1/security/PasswordSecurity.java
+++ src/main/java/com/newneek/j1/security/PasswordSecurity.java
@@ -8,22 +8,29 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity //활성화
-public class PasswordSecurity extends WebSecurityConfigurerAdapter{
-
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-        http
-        .csrf().disable() //스프링 시큐리티의 CSRF 보호 기능을 비활성화
-        				  //CSRF(Cross Site Request Forgery : 사이트 간 요청 위조)
-        
-        .cors().disable() //CORS(Cross Origin Resource Sharing : 도메인 간 통신을 허용)
-        .headers().frameOptions().disable();
+public class PasswordSecurity implements PasswordEncoder{
+	
+	private PasswordEncoder passwordEncoder;
+	
+	public PasswordSecurity() {
+		this.passwordEncoder = new BCryptPasswordEncoder();
 	}
 	
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
+	public PasswordSecurity(PasswordEncoder passwordEncoder) {
+		this.passwordEncoder = passwordEncoder;
+		
 	}
+
+	@Override
+	public String encode(CharSequence rawPassword) {
+		return passwordEncoder.encode(rawPassword);
+	}
+
+	@Override
+	public boolean matches(CharSequence rawPassword, String encodedPassword) {
+		return passwordEncoder.matches(rawPassword, encodedPassword);
+	}
+	
 	
 	
 
